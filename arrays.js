027298d9e321106f84b2fa6204eac4937bedeecb
Shanmuk <shanmukha.anipaddi@adsiduous.com>
2022-07-06 08:42:15 -0700

    min number with functions

--- arrays.js
+++ arrays.js
@@ -1,3 +1,10 @@
 ////////////////////
 //      Array     //
 ///////////////////
+
+let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,12];
+
+///////Find the smallest number 
+
+const min = Math.min(...numbers);
+console.log(min);
