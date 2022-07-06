5d1b65ad1aa4d53afc7ed1fe1a0df19d63d2aa75
Shanmuk <shanmukha.anipaddi@adsiduous.com>
2022-07-06 08:52:16 -0700

    Biggest number with for loop

--- arrays.js
+++ arrays.js
@@ -2,18 +2,27 @@
 //      Array     //
 ///////////////////
 
-let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,12];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 12];
 let numberCount = numbers.length;
-///////Find the smallest number 
+///////Find the smallest number
 
 // const min = Math.min(...numbers);
 // console.log(min);
 
 //Finding the smallest numbers
-var smallest = numbers[0];
-for(i=0;i<= numberCount;i++){
-    if(numbers[i] < smallest){
-        smallest = numbers[i];
-    }
+// var smallest = numbers[0];
+// for(i=0;i<= numberCount;i++){
+//     if(numbers[i] < smallest){
+//         smallest = numbers[i];
+//     }
+// }
+// console.log(smallest);
+
+//Finding the Biggest numbers
+var biggest = numbers[0];
+for (i = 0; i <= numberCount; i++) {
+  if (numbers[i] > biggest) {
+    biggest = numbers[i];
+  }
 }
-console.log(smallest);
+console.log(biggest);
