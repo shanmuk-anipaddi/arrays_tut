572c56ba43dc45ba2fa1648890e3033263c39ac1
Shanmuk <shanmukha.anipaddi@adsiduous.com>
2022-07-06 08:49:54 -0700

    smallest number with for loop

--- arrays.js
+++ arrays.js
@@ -3,8 +3,17 @@
 ///////////////////
 
 let numbers = [1,2,3,4,5,6,7,8,9,10,11,12,12];
-
+let numberCount = numbers.length;
 ///////Find the smallest number 
 
-const min = Math.min(...numbers);
-console.log(min);
+// const min = Math.min(...numbers);
+// console.log(min);
+
+//Finding the smallest numbers
+var smallest = numbers[0];
+for(i=0;i<= numberCount;i++){
+    if(numbers[i] < smallest){
+        smallest = numbers[i];
+    }
+}
+console.log(smallest);
