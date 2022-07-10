30501a17f3cac61350bee9c2f5980301e17e4776
Shanmuk <shanmukha.anipaddi@adsiduous.com>
2022-07-10 07:41:53 -0700

    with for of

--- loop.js
+++ loop.js
@@ -1,10 +1,38 @@
 // for loop
 
-const person = {fname:"John", lname:"Doe", age:25};
+// const person = {fname:"John", lname:"Doe", age:25};
 
-let text = '';
+// let text = '';
 
-for(let x in person){
-    //console.log(x);
-    console.log(x+' = '+person[x]);
+// for(let x in person){
+//     //console.log(x);
+//     console.log(x+' = '+person[x]);
+// }
+
+
+//For in over arrays
+
+const numbers = [4,10,2,89,45]
+
+// let sum ;
+// for(let x in numbers ){
+//     console.log(x);
+// }
+
+///Foreach of Array
+
+// let txt = '';
+// numbers.forEach(myFuntion);
+
+// function myFuntion(value,index,array){
+//     console.log("Value :"+value)
+//     console.log("Index :"+index)
+//     //console.log("array :",array)
+// }
+
+//      For of            //
+for(let num of numbers){
+    console.log(num);
 }
+
+
